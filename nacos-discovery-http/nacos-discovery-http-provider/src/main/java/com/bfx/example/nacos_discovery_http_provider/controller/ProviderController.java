db4509a89e957bc46052478ddbcc7d351623324c
tony-coder <1606771552@qq.com>
2023-06-18 22:24:51 +0800

    完善方法

--- nacos-discovery-http/nacos-discovery-http-provider/src/main/java/com/bfx/example/nacos_discovery_http_provider/controller/ProviderController.java
+++ nacos-discovery-http/nacos-discovery-http-provider/src/main/java/com/bfx/example/nacos_discovery_http_provider/controller/ProviderController.java
@@ -1,11 +1,22 @@
 package com.bfx.example.nacos_discovery_http_provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProviderController {
 
+    @Value("${server.port}")
+    private int port;
+
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping(value = "sayHello")
+    public String sayHello(@RequestParam String world) {
+        return "当前服务端口为：" + port + "，请求参数：" + world;
     }
 }
